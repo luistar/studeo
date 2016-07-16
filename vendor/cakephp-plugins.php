79b0f12592d5c4a81d1900dfe2e4064ea87027b3
Luigi Starace <luigi.starace@gmail.com>
2016-07-16 10:17:43 +0200

    Included vendor directory, config/app.php in versioning.

---
+++ vendor/cakephp-plugins.php
@@ -0,0 +1,10 @@
+<?php
+$baseDir = dirname(dirname(__FILE__));
+return [
+    'plugins' => [
+        'Bake' => $baseDir . '/vendor/cakephp/bake/',
+        'Bootstrap' => $baseDir . '/vendor/holt59/cakephp3-bootstrap-helpers/',
+        'DebugKit' => $baseDir . '/vendor/cakephp/debug_kit/',
+        'Migrations' => $baseDir . '/vendor/cakephp/migrations/'
+    ]
+];
