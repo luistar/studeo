887cff837e567d4ffc08b07c00aa2d8db84f93f1
Luigi Starace <luigi.starace@gmail.com>
2017-02-10 16:16:31 +0100

    Created helper to be used (i.e. for printing links to exams etc.)

---
+++ src/View/Helper/StudeoHelper.php
@@ -0,0 +1,16 @@
+<?php 
+namespace App\View\Helper;
+
+use Cake\View\Helper;
+use App\Model\Entity\Exam;
+
+class StudeoHelper extends Helper
+{
+	/**
+	 * Displays an exam as a list-group item
+	 * @param Exam $exam
+	 */
+    public function displayExamsAsListGroup(Array $exams) {
+    	
+    }
+}
