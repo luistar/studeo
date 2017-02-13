a64f0ef1ad097ef678719baf6d29a480276a8b9d
Luigi Starace <luigi.starace@gmail.com>
2017-02-13 08:00:34 +0100

    Added phpbb_db source in default.app.php

--- config/app.default.php
+++ config/app.default.php
@@ -264,6 +264,44 @@ return [
 
             'url' => env('DATABASE_URL', null),
         ],
+    		
+    	/**
+    	 * Connection to the phpbb database.
+    	 */
+    	'phpbb_db' => [
+    		'className' => 'Cake\Database\Connection',
+    		'driver' => 'Cake\Database\Driver\Mysql',
+    		'persistent' => false,
+    		'host' => 'localhost',
+    		//'port' => 'non_standard_port_number',
+    		'username' => 'phpbb',
+    		'password' => 'phpbb',
+    		'database' => 'phpbb',
+    		'encoding' => 'utf8',
+    		'timezone' => 'UTC',
+    		'flags' => [],
+    		'cacheMetadata' => true,
+    		'log' => false,
+    	/**
+    	 * Set identifier quoting to true if you are using reserved words or
+    	 * special characters in your table or column names. Enabling this
+    	 * setting will result in queries built using the Query Builder having
+    	 * identifiers quoted when creating SQL. It should be noted that this
+    	 * decreases performance because each query needs to be traversed and
+    	 * manipulated before being executed.
+    	 */
+    		'quoteIdentifiers' => false,
+    	/**
+    	 * During development, if using MySQL < 5.6, uncommenting the
+    	 * following line could boost the speed at which schema metadata is
+    	 * fetched from the database. It can also be set directly with the
+    	 * mysql configuration directive 'innodb_stats_on_metadata = 0'
+    	 * which is the recommended value in production environments
+         */
+    	    //'init' => ['SET GLOBAL innodb_stats_on_metadata = 0'],
+    		'url' => env('DATABASE_URL', null),
+    		'phpbbTablePrefix' => 'phpbb',
+    	],
 
         /**
          * The test connection is used during the test suite.
