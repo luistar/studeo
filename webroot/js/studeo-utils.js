50e073ba2bff3e70eb94745e38a9668a5b014a91
Luigi Starace <luigi.starace@gmail.com>
2017-02-16 10:04:11 +0100

    Fixed bug where redirection didn't work

--- webroot/js/studeo-utils.js
+++ webroot/js/studeo-utils.js
@@ -1,6 +1,7 @@
 $(document).ready( function(){
 	$('.link-button').click(function(event){
 		event.stopPropagation(); //we do not want the event to toggle the solution list
+		event.preventDefault();
 		document.location.href = $(this).attr('data-link'); //go to page
 	});
 	
